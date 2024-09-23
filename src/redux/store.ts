883c174babe65e6bf0586a09ee6ad654b51963f2
Bishal Giri <bsaal2@Bishals-MacBook-Pro.local>
2024-09-23 20:37:40 +0900

    feat: add multiple middleware

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,12 +2,13 @@ import { legacy_createStore as createStore, combineReducers, applyMiddleware, Di
 import addReducer from "./reducer/addReducer.ts";
 import minusReducer from "./reducer/minusReducer.ts";
 import myMiddleware from "./middleware/myMiddleware.ts";
+import myNextMiddleware from "./middleware/myNextMiddleware.ts";
 
 const appReducer = combineReducers({
     add: addReducer,
     minus: minusReducer,
 })
 
-const store = createStore(appReducer, applyMiddleware(myMiddleware));
+const store = createStore(appReducer, applyMiddleware(myMiddleware, myNextMiddleware));
 
 export default store;
