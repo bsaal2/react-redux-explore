39b6e64d1365d369c68a35ba357ca174c5c4bb2d
Bishal Giri <bsaal2@Bishals-MacBook-Pro.local>
2024-09-26 17:13:30 +0900

    feat: dispatch the action in the UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
 import { useState, useEffect } from 'react';
 import store from './redux/store.ts';
-import { increaseCount, decreaseCount } from './redux/action/countAction.ts';
+import { increaseCountInitiate, decreaseCountInitiate } from './redux/action/countAction.ts';
+
+const Loading = () => (
+    <span>Loading...</span>
+)
 
 function App() {
-    const [addCount, setAddCount] = useState(store.getState().add?.addCount);
-    const [minusCount, setMinusCount] = useState(store.getState().minus.minusCount);
+    const [addObj, setAddObj] = useState(store.getState().add);
+    const [minusObj, setMinusObj] = useState(store.getState().minus);
+
+    const addLoading = addObj?.loading;
+    const minusLoading = minusObj?.loading;
 
   const onIncreaseHandler = () => {
-      store.dispatch(increaseCount());
+      store.dispatch(increaseCountInitiate());
   }
 
   const onDecreaseHandler = () => {
-      store.dispatch(decreaseCount());
+      store.dispatch(decreaseCountInitiate());
   }
 
     useEffect(() => {
         const unsubscribe = store.subscribe(() => {
-            setAddCount(store.getState().add?.addCount);
-            setMinusCount(store.getState().minus.minusCount);
+            console.log('entered');
+            setAddObj(store.getState().add);
+            setMinusObj(store.getState().minus);
         });
 
         return () => {
@@ -27,8 +35,15 @@ function App() {
 
   return (
     <section id="myCountApp">
-        <p>Add Count: { addCount }</p>
-        <p>Minus Count: { minusCount }</p>
+        <p>Add Count:
+            { addLoading && <Loading /> }
+            { !addLoading && <span>{ addObj?.addCount }</span> }
+        </p>
+
+        <p>Minus Count:
+            { minusLoading && <Loading /> }
+            { !minusLoading && <span>{ minusObj?.minusCount }</span> }
+        </p>
         <button onClick={onIncreaseHandler}>Add count</button>
         <button onClick={onDecreaseHandler}>Decrease count</button>
     </section>
